c7281a285437499bde0afd573e20d48e662fc9c0
nosnhojn <nosnhojn@gmail.com>
2014-05-14 16:49:15 -0600

    refactored

--- sw/test/iicCtrlTest.cpp
+++ sw/test/iicCtrlTest.cpp
@@ -1,7 +1,16 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
 #include "IicCtrlMock.h"
 #include "xdriverMock.h"
 
 using namespace testing;
+
+#define XIIC_FIFOS_EMPTY          XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK
+#define XIIC_FIFOS_EMPTY_AND_BUSY XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK | XIIC_SR_BUS_BUSY_MASK
+#define XIIC_BASE_ADDR            iicCtrl->getHdmiI2cBaseAddr()
+#define XIIC_SR_REG               XIIC_BASE_ADDR + XIIC_SR_REG_OFFSET
+
 
 class IicCtrlTest : public testing::Test
 {
@@ -13,8 +22,8 @@ class IicCtrlTest : public testing::Test
     {
       iicCtrl = new IicCtrl();
       xdMock = getXdriverMock();
-      ON_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET))
-          .WillByDefault(Return(XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK));
+      ON_CALL(*xdMock, Xil_In8(XIIC_SR_REG))
+          .WillByDefault(Return(XIIC_FIFOS_EMPTY));
     }
 
     ~IicCtrlTest()
@@ -24,60 +33,60 @@ class IicCtrlTest : public testing::Test
 };
 
 TEST_F(IicCtrlTest, dynamicInit) {
-  EXPECT_CALL(*xdMock, XIic_DynInit(iicCtrl->getHdmiI2cBaseAddr())).Times(1);
+  EXPECT_CALL(*xdMock, XIic_DynInit(XIIC_BASE_ADDR)).Times(1);
 
   iicCtrl->init();
 }
 
 TEST_F(IicCtrlTest, dynamicInitSuccess) {
-  EXPECT_CALL(*xdMock, XIic_DynInit(iicCtrl->getHdmiI2cBaseAddr())).WillOnce(Return(XST_SUCCESS));
+  EXPECT_CALL(*xdMock, XIic_DynInit(XIIC_BASE_ADDR)).WillOnce(Return(XST_SUCCESS));
 
   EXPECT_TRUE(iicCtrl->init());
 }
 
 TEST_F(IicCtrlTest, dynamicInitFailure) {
-  EXPECT_CALL(*xdMock, XIic_DynInit(iicCtrl->getHdmiI2cBaseAddr())).WillOnce(Return(XST_FAILURE));
+  EXPECT_CALL(*xdMock, XIic_DynInit(XIIC_BASE_ADDR)).WillOnce(Return(XST_FAILURE));
 
   EXPECT_FALSE(iicCtrl->init());
 }
 
 TEST_F(IicCtrlTest, InitWithRxStatusFifosNonEmpty) {
-  EXPECT_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillOnce(Return(XIIC_SR_TX_FIFO_EMPTY_MASK))
-                                                                                   .WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK));
+  EXPECT_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillOnce(Return(XIIC_SR_TX_FIFO_EMPTY_MASK))
+                                            .WillOnce(Return(XIIC_FIFOS_EMPTY));
 
   iicCtrl->init();
 }
 
 TEST_F(IicCtrlTest, InitWithTxStatusFifosNonEmpty) {
-  EXPECT_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK))
-                                                                                   .WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK));
+  EXPECT_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK))
+                                            .WillOnce(Return(XIIC_FIFOS_EMPTY));
 
   iicCtrl->init();
 }
 
 TEST_F(IicCtrlTest, InitWithFifoStatusPolling) {
-  EXPECT_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK))
-                                                                                   .WillOnce(Return(XIIC_SR_TX_FIFO_EMPTY_MASK))
-                                                                                   .WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK));
+  EXPECT_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK))
+                                            .WillOnce(Return(XIIC_SR_TX_FIFO_EMPTY_MASK))
+                                            .WillOnce(Return(XIIC_FIFOS_EMPTY));
   iicCtrl->init();
 }
 
 TEST_F(IicCtrlTest, InitWithBusy) {
-  EXPECT_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK | XIIC_SR_BUS_BUSY_MASK))
-                                                                                   .WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK));
+  EXPECT_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillOnce(Return(XIIC_FIFOS_EMPTY_AND_BUSY))
+                                            .WillOnce(Return(XIIC_FIFOS_EMPTY));
   iicCtrl->init();
 }
 
 TEST_F(IicCtrlTest, InitTimeout) {
-  ON_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillByDefault(Return(0));
+  ON_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillByDefault(Return(0));
 
-  EXPECT_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).Times(IIC_STATUS_TIMEOUT);
+  EXPECT_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).Times(IIC_STATUS_TIMEOUT);
 
   iicCtrl->init();
 }
 
 TEST_F(IicCtrlTest, InitFailOnTimeout) {
-  ON_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillByDefault(Return(0));
+  ON_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillByDefault(Return(0));
 
   EXPECT_FALSE(iicCtrl->init());
 }
@@ -85,10 +94,10 @@ TEST_F(IicCtrlTest, InitFailOnTimeout) {
 TEST_F(IicCtrlTest, InitSuccessOnNearTimeout) {
   InSequence dummy;
 
-  ON_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillByDefault(Return(0));
+  ON_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillByDefault(Return(0));
 
-  EXPECT_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).Times(IIC_STATUS_TIMEOUT-1);
-  EXPECT_CALL(*xdMock, Xil_In8(iicCtrl->getHdmiI2cBaseAddr() + XIIC_SR_REG_OFFSET)).WillOnce(Return(XIIC_SR_RX_FIFO_EMPTY_MASK | XIIC_SR_TX_FIFO_EMPTY_MASK));
+  EXPECT_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).Times(IIC_STATUS_TIMEOUT-1);
+  EXPECT_CALL(*xdMock, Xil_In8(XIIC_SR_REG)).WillOnce(Return(XIIC_FIFOS_EMPTY));
 
   EXPECT_TRUE(iicCtrl->init());
 }
