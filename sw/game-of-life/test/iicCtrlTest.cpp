5cb619873169b3fbccb8e1cf9948405b7446f77f
nosnhojn <nosnhojn@gmail.com>
2014-06-25 22:40:30 -0600

    added the carrier init test back in

--- sw/game-of-life/test/iicCtrlTest.cpp
+++ sw/game-of-life/test/iicCtrlTest.cpp
@@ -7,6 +7,49 @@
 
 using namespace testing;
 
+Xuint8 my_carrier_hdmi_out_config[CARRIER_HDMI_OUT_CONFIG_LEN][3] = {
+  {ADV7511_ADDR>>1, 0x15, 0x01},
+  {ADV7511_ADDR>>1, 0x16, 0x38},
+  {ADV7511_ADDR>>1, 0x18, 0xAC},
+  {ADV7511_ADDR>>1, 0x19, 0x53},
+  {ADV7511_ADDR>>1, 0x1A, 0x08},
+  {ADV7511_ADDR>>1, 0x1B, 0x00},
+  {ADV7511_ADDR>>1, 0x1C, 0x00},
+  {ADV7511_ADDR>>1, 0x1D, 0x00},
+  {ADV7511_ADDR>>1, 0x1E, 0x19},
+  {ADV7511_ADDR>>1, 0x1F, 0xD6},
+  {ADV7511_ADDR>>1, 0x20, 0x1C},
+  {ADV7511_ADDR>>1, 0x21, 0x56},
+  {ADV7511_ADDR>>1, 0x22, 0x08},
+  {ADV7511_ADDR>>1, 0x23, 0x00},
+  {ADV7511_ADDR>>1, 0x24, 0x1E},
+  {ADV7511_ADDR>>1, 0x25, 0x88},
+  {ADV7511_ADDR>>1, 0x26, 0x02},
+  {ADV7511_ADDR>>1, 0x27, 0x91},
+  {ADV7511_ADDR>>1, 0x28, 0x1F},
+  {ADV7511_ADDR>>1, 0x29, 0xFF},
+  {ADV7511_ADDR>>1, 0x2A, 0x08},
+  {ADV7511_ADDR>>1, 0x2B, 0x00},
+  {ADV7511_ADDR>>1, 0x2C, 0x0E},
+  {ADV7511_ADDR>>1, 0x2D, 0x85},
+  {ADV7511_ADDR>>1, 0x2E, 0x18},
+  {ADV7511_ADDR>>1, 0x2F, 0xBE},
+  {ADV7511_ADDR>>1, 0x41, 0x10},
+  {ADV7511_ADDR>>1, 0x48, 0x08},
+  {ADV7511_ADDR>>1, 0x55, 0x00},
+  {ADV7511_ADDR>>1, 0x56, 0x28},
+  {ADV7511_ADDR>>1, 0x98, 0x03},
+  {ADV7511_ADDR>>1, 0x9A, 0xE0},
+  {ADV7511_ADDR>>1, 0x9C, 0x30},
+  {ADV7511_ADDR>>1, 0x9D, 0x61},
+  {ADV7511_ADDR>>1, 0xA2, 0xA4},
+  {ADV7511_ADDR>>1, 0xA3, 0xA4},
+  {ADV7511_ADDR>>1, 0xAF, 0x04},
+  {ADV7511_ADDR>>1, 0xBA, 0x60},
+  {ADV7511_ADDR>>1, 0xE0, 0xD0},
+  {ADV7511_ADDR>>1, 0xF9, 0x00}
+};
+
 class IicCtrlTest : public testing::Test
 {
   public:
@@ -137,8 +180,8 @@ TEST_F(IicCtrlTest, writeBufferEndsWithBufferPtr) {
 
 TEST_F(IicCtrlTest, initHdmiCfgAll) {
   for (int i=0; i<CARRIER_HDMI_OUT_CONFIG_LEN; i++) {
-    EXPECT_CALL(*xdMock, XIic_DynSend(HDMI_I2C_BASE_ADDR, 0x39, _, 2, XIIC_STOP)).With(Args<2,3>(ElementsAre(IicCtrl::carrier_hdmi_out_config[i][1],
-                                                                                                                                 IicCtrl::carrier_hdmi_out_config[i][2])));
+    EXPECT_CALL(*xdMock, XIic_DynSend(HDMI_I2C_BASE_ADDR, 0x39, _, 2, XIIC_STOP)).With(Args<2,3>(ElementsAre(my_carrier_hdmi_out_config[i][1],
+                                                                                                             my_carrier_hdmi_out_config[i][2])));
   }
 
   iicCtrl->init();
