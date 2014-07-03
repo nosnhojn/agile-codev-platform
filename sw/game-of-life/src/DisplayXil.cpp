786dbee269a2f62cfaa04cf8bed9e4ed9908b052
nosnhojn <soheil.salehian@gmail.com>
2014-07-02 21:05:06 -0600

    big changes, let the game begin.

--- sw/game-of-life/src/DisplayXil.cpp
+++ sw/game-of-life/src/DisplayXil.cpp
@@ -2,51 +2,47 @@
 #include <iostream>
 #include <cstring>
 
-DisplayXil::DisplayXil( IicCtrl * iicCtrl = 0,
-                        Xuint32 HDMI_DISPLAY_MEM_BASE_ADDR = 0,
-                        Xuint32 HDMI_VTC_DEVICE_ID = 0,
-                        Xuint32 HDMI_VDMA_DEVICE_ID = 0) :
-  m_iicCtrl(iicCtrl),
-  m_HdmiDisplayMemBaseAddr(HDMI_DISPLAY_MEM_BASE_ADDR),
-  m_HdmiVtcDeviceId(HDMI_VTC_DEVICE_ID),
-  m_HdmiVdmaDeviceId(HDMI_VDMA_DEVICE_ID),
+
+Xuint32 color;
+//test
+DisplayXil::DisplayXil( DisplayXilCfg * cfg ) :
+  m_cfg(cfg),
   m_width(1920),
   m_height(1080),
-  m_resolution(VIDEO_RESOLUTION_1080P),
   m_gridHeight(0),
   m_gridWidth(0),
+  m_resolution(VIDEO_RESOLUTION_1080P),
   m_xvtcEnGenerator(XVTC_EN_GENERATOR)
 {
+	color = 0x00000000;
 }
 
 int DisplayXil::_initscr()
 {
-  if (m_iicCtrl->init() == 0) return 0;
+  if (m_cfg->iicCtrl->init() == 0) return 0;
 
   _clear();
 
-  if (vfb_common_init(getHdmiVdmaDeviceId(), getAxiVdma()) == 1) return 0;
+  if (vfb_common_init(m_cfg->vdmaId, &(m_cfg->axiVdma)) == 1) return 0;
 
-  if (vfb_tx_init(getAxiVdma(),
-                  getAxiVdmaCfg(),
+  if (vfb_tx_init(&(m_cfg->axiVdma),
+                  &(m_cfg->axiVdmaCfg),
                   VIDEO_RESOLUTION_1080P,
                   VIDEO_RESOLUTION_1080P,
-                  getHdmiDisplayMemBaseAddr(),
-                  1)) return 0;
+                  m_cfg->hdmiDisplayMemBaseAddr,
+                  3)) return 0;
 
-
-  XVtc pVtc;
-  if (vgen_init(&pVtc, getHdmiVtcDeviceId()) != 0) return 0;
-  vgen_config(&pVtc, getResolution(), 0);
+  if (vgen_init(&(m_cfg->vtc), m_cfg->vtcId) != 0) return 0;
+  vgen_config(&(m_cfg->vtc), getResolution(), 0);
 
   return 1;
 }
 
 void DisplayXil::_clear()
 {
-  volatile Xuint32 *mem = (Xuint32 *)getHdmiDisplayMemBaseAddr();
+  volatile Xuint32 *mem = (Xuint32 *)(m_cfg->hdmiDisplayMemBaseAddr);
 
-  for (Xuint32 r=0; r<getHeight(); r++) {
+  for (Xuint32 r=0; r<getHeight()*3; r++) {
     for (Xuint32 c=0; c<getWidth(); c++) {
       *mem++ = getBgColour();
     }
@@ -59,14 +55,14 @@ void DisplayXil::_endwin()
 
 void DisplayXil::_refresh()
 {
-  vfb_tx_stop(getAxiVdma());
+//  vfb_tx_stop(&(m_cfg->axiVdma));
 
-  if (m_gridWidth > 0) m_writeGridToFrameBuffer();
+  m_writeGridToFrameBuffer();
   m_resetGrid();
 
-  vfb_tx_start(getAxiVdma());
+//  vfb_tx_start(&(m_cfg->axiVdma));
 
-  m_iicCtrl->carrierInit();
+//  m_cfg->iicCtrl->carrierInit();
 }
 
 Xuint32 DisplayXil::getLiveCellPixelWithCoords(Xuint32 x, Xuint32 y) {
@@ -111,36 +107,6 @@ int DisplayXil::getResolution()
   return m_resolution;
 }
 
-Xuint32 DisplayXil::getHdmiVtcDeviceId()
-{
-  return m_HdmiVtcDeviceId;
-}
-
-Xuint32 DisplayXil::getHdmiVdmaDeviceId()
-{
-  return m_HdmiVdmaDeviceId;
-}
-
-Xuint32 DisplayXil::setHdmiDisplayMemBaseAddr(Xuint32 addr)
-{
-  m_HdmiDisplayMemBaseAddr = addr;
-}
-
-Xuint32 DisplayXil::getHdmiDisplayMemBaseAddr()
-{
-  return m_HdmiDisplayMemBaseAddr;
-}
-
-XAxiVdma * DisplayXil::getAxiVdma()
-{
-  return &m_axiVdma;
-}
-
-XAxiVdma_DmaSetup * DisplayXil::getAxiVdmaCfg()
-{
-  return &m_axiVdmaCfg;
-}
-
 Xuint32 DisplayXil::m_rowIndexFromYPixelCoord(Xuint32 y_coord)
 {
   return (Xuint32) (y_coord/(getHeight()/m_gridHeight));
@@ -156,23 +122,20 @@ char DisplayXil::m_charAtCoord(Xuint32 x_coord, Xuint32 y_coord)
   return m_charGrid[m_rowIndexFromYPixelCoord(y_coord)][m_columnIndexFromXPixelCoord(x_coord)];
 }
 
+
 void DisplayXil::m_writeGridToFrameBuffer()
 {
-  volatile Xuint32 *mem = (Xuint32 *)getHdmiDisplayMemBaseAddr();
-
+  volatile Xuint32 *mem = (Xuint32 *)m_cfg->hdmiDisplayMemBaseAddr;
 
   // HEY SOHEIL! Comment this out and uncomment the stuff below for a solid colour screen
-  for (Xuint32 y=0; y<getHeight(); y++) {
-    for (Xuint32 x=0; x<getWidth(); x++) {
-      if (m_charAtCoord(x, y) != ' ') *mem++ = getLiveCellPixelWithCoords(getCellXCoord(x, m_gridWidth), getCellXCoord(y, m_gridHeight));
-      else  *mem++ = getBgColour();
+for (int j=0; j<3; j+=1) {
+	for (Xuint32 y=0; y<getHeight(); y++) {
+      for (Xuint32 x=0; x<getWidth(); x++) {
+         if (m_charAtCoord(x, y) != ' ') *mem++ = getLiveCellPixelWithCoords(getCellXCoord(x, m_gridWidth), getCellXCoord(y, m_gridHeight));
+         else  *mem++ = getBgColour();
+      }
     }
-  }
-// for (Xuint32 y=0; y<getHeight(); y++) {
-//   for (Xuint32 x=0; x<getWidth(); x++) {
-//     *mem++ = 0x333333;
-//   }
-// }
+}
 
   Xil_DCacheFlush();
 }
@@ -211,4 +174,29 @@ XVtc_Signal * DisplayXil::getXvtcSignal()
 XVtc_SourceSelect * DisplayXil::getXvtcSourceSelect()
 {
   return &m_sourceSelect;
+}
+
+void DisplayXil::printAxiVdma() {
+  xil_printf(
+      "\n\nfull getAxiVdma()->BaseAddr: %0x\n, \
+      EnableVIDParamRead: %0x\n, HasMm2S: %0x\n, \
+      HasMm2SDRE: %0x\n, \
+      HasS2MmDRE: %0x\n, \
+      IsReady: %0x\n\n, \
+      HasS2Mm: 0x%0x\n \
+      HasSG: 0x%0x\n \
+      InternalGenLock: 0x%0x\n \
+      MaxNumFrames: 0x%0x\n \
+      UseFsync: 0x%0x\n",
+      m_cfg->axiVdma.BaseAddr, \
+      m_cfg->axiVdma.EnableVIDParamRead,
+      m_cfg->axiVdma.HasMm2S, \
+      m_cfg->axiVdma.HasMm2SDRE,
+      m_cfg->axiVdma.HasS2MmDRE, \
+      m_cfg->axiVdma.IsReady,
+      m_cfg->axiVdma.HasS2Mm, \
+      m_cfg->axiVdma.HasSG, \
+      m_cfg->axiVdma.InternalGenLock, \
+      m_cfg->axiVdma.MaxNumFrames, \
+      m_cfg->axiVdma.UseFsync);
 }
