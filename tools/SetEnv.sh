3ae07be7740f7fe905c76ce6b1b7040db8c447ef
nosnhojn <soheil.salehian@gmail.com>
2014-07-25 15:42:31 -0600

    rm'd old version of hw

--- tools/SetEnv.sh
+++ tools/SetEnv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 googlemock
-export DUT_DIR=/home/agilehw/agile-codev-platform/hw/Vivado/agileHWBlock_10/agileHWBlock.srcs/sources_1/imports/new
+export DUT_DIR=/home/agilehw/agile-codev-platform/hw/Vivado/hwIPBlock/hwIPBlock.srcs/sources_1/new/
 export PS1='\[\033[0;35m\]\h\[\033[0;33m\] \w\[\033[00m\]: '
 export SVUNIT_INSTALL=/home/agilehw/svunit-code/
 echo 'include $(SVUNIT_INSTALL)/bin/modelsim.mk' > svunit.mk
