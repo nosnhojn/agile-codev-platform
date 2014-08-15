abf8c02d7b760aafb429233b073f86f82dcfb173
nosnhojn <nosnhojn@gmail.com>
2014-08-15 18:13:51 -0400

    port1 checkpoint

--- hw/Vivado/hwIPBlock/hwIPBlock.srcs/sources_1/new/dpram.v
+++ hw/Vivado/hwIPBlock/hwIPBlock.srcs/sources_1/new/dpram.v
@@ -1,21 +1,47 @@
 module dpram
+#(
+  SIZE = 16
+)
 (
   input clk,
   input rst_n,
 
-  input wdata_0,
-  input waddr_0,
+  input [31:0] wdata_0,
+  input [31:0] waddr_0,
   input wr_0,
 
-  input wdata_1,
-  input waddr_1,
+  input [31:0] wdata_1,
+  input [31:0] waddr_1,
   input wr_1,
 
-  output logic rdata_0,
-  input raddr_0,
+  output logic [31:0] rdata_0,
+  input        [31:0] raddr_0,
 
-  output logic rdata_1,
-  input raddr_1
+  output logic [31:0] rdata_1,
+  input        [31:0] raddr_1
 );
+
+logic [31:0] mem [SIZE-1:0];
+
+always @(negedge rst_n or posedge clk) begin
+  if (!rst_n) begin
+    for (int i=0; i<SIZE; i++) begin
+      mem[i] <= 'hx;
+    end
+  end
+
+  else begin
+    rdata_0 <= mem[raddr_0];
+    rdata_1 <= mem[raddr_1];
+
+    if (wr_0) begin
+      mem[waddr_0] <= wdata_0;
+    end
+
+    if (wr_1) begin
+      mem[waddr_1] <= wdata_1;
+    end
+  end
+end
 
 endmodule
--- hw/Vivado/hwIPBlock/hwIPBlock.srcs/sources_1/new/unitTest/dpram_unit_test.sv
+++ hw/Vivado/hwIPBlock/hwIPBlock.srcs/sources_1/new/unitTest/dpram_unit_test.sv
@@ -1,36 +1,111 @@
 `include "svunit_defines.svh"
 `include "dpram.v"
 
+`define TEST_DPRAM_SIZE 256
+
+`define write_read_port_N_test(PORT) \
+`SVTEST(write_read_port_``PORT) \
+  write_``PORT('hf, 'haabbccdd); \
+  step(); \
+  read_``PORT('hf); \
+  step(); \
+  expect_rdata_``PORT('haabbccdd); \
+`SVTEST_END
+
+`define rdata_N_is_registered_test(PORT) \
+`SVTEST(rdata_``PORT``_is_registered) \
+  write_``PORT('hf, 'haabbccdd); \
+  step(); \
+  read_``PORT('hf); \
+  expect_rdata_``PORT('hx); \
+`SVTEST_END
+
+`define no_write_N_in_reset(PORT) \
+`SVTEST(no_write_``PORT``_in_reset) \
+  rst_n = 0; \
+  write_``PORT('hf, 'haabbccdd); \
+  step(); \
+  read_``PORT('hf); \
+  rst_n = 1; \
+  step(); \
+  expect_rdata_``PORT('hx); \
+`SVTEST_END
+
+`define no_read_N_in_reset(PORT) \
+`SVTEST(no_read_``PORT``_in_reset) \
+  write_``PORT('hf, 'haabbccdd); \
+  step(); \
+  read_``PORT('hf); \
+  rst_n = 0; \
+  step(); \
+  expect_rdata_``PORT('hx); \
+`SVTEST_END
+
+`define write_read_port_N_full_range(PORT) \
+`SVTEST(write_read_port_``PORT``_full_range) \
+  for (int i=0; i<`TEST_DPRAM_SIZE; i++) begin \
+    write_``PORT(i, i); \
+    step(); \
+  end \
+  for (int i=0; i<`TEST_DPRAM_SIZE; i++) begin \
+    read_``PORT(i); \
+    step(); \
+    expect_rdata_``PORT(i); \
+  end \
+`SVTEST_END
+
 module dpram_unit_test;
   import svunit_pkg::svunit_testcase;
 
   string name = "dpram_ut";
   svunit_testcase svunit_ut;
 
+  `define PORT0 0
+  `define PORT1 1
+
   logic clk;
   logic rst_n;
+
+  logic [31:0] wdata_0;
+  logic [31:0] waddr_0;
+  logic wr_0;
+
+  logic [31:0] wdata_1;
+  logic [31:0] waddr_1;
+  logic wr_1;
+
+  wire  [31:0] rdata_0;
+  logic [31:0] raddr_0;
+
+  wire  [31:0] rdata_1;
+  logic [31:0] raddr_1;
 
   //===================================
   // This is the UUT that we're 
   // running the Unit Tests on
   //===================================
-  dpram my_dpram(
+  dpram
+  #(
+    .SIZE(`TEST_DPRAM_SIZE)
+  )
+  my_dpram
+  (
     .clk(clk),
     .rst_n(rst_n),
 
-    .wdata_0(),
-    .waddr_0(),
-    .wr_0(),
+    .wdata_0(wdata_0),
+    .waddr_0(waddr_0),
+    .wr_0(wr_0),
 
-    .wdata_1(),
-    .waddr_1(),
-    .wr_1(),
+    .rdata_0(rdata_0),
+    .raddr_0(raddr_0),
 
-    .rdata_0(),
-    .raddr_0(),
+    .wdata_1(wdata_1),
+    .waddr_1(waddr_1),
+    .wr_1(wr_1),
 
-    .rdata_1(),
-    .raddr_1()
+    .rdata_1(rdata_1),
+    .raddr_1(raddr_1)
   );
 
   initial begin
@@ -38,10 +113,46 @@ module dpram_unit_test;
     rst_n = 1;
   end
 
-  task step(int size = 1);
+  task automatic step(int size = 1);
     repeat (size) begin
-      repeat (2) #5 clk = ~clk;
+      int step_size = 5 - $time % 5;
+      #(step_size) clk = ~clk;
+      #5 clk = ~clk;
     end
+  endtask
+
+  task expect_rdata_0(logic [31:0] exp);
+    if ($time%5 == 0) #1;
+    `FAIL_UNLESS(rdata_0 === exp);
+  endtask
+
+  task write_0(bit [31:0] addr, bit [31:0] data);
+    if ($time%5 == 0) #1;
+    wr_0 = 1;
+    waddr_0 = addr;
+    wdata_0 = data;
+  endtask
+
+  task read_0(bit [31:0] addr);
+    if ($time%5 == 0) #1;
+    raddr_0 = addr;
+  endtask
+
+  task expect_rdata_1(logic [31:0] exp);
+    if ($time%5 == 0) #1;
+    `FAIL_UNLESS(rdata_1 === exp);
+  endtask
+
+  task write_1(bit [31:0] addr, bit [31:0] data);
+    if ($time%5 == 0) #1;
+    wr_1 = 1;
+    waddr_1 = addr;
+    wdata_1 = data;
+  endtask
+
+  task read_1(bit [31:0] addr);
+    if ($time%5 == 0) #1;
+    raddr_1 = addr;
   endtask
 
 
@@ -58,7 +169,27 @@ module dpram_unit_test;
   //===================================
   task setup();
     svunit_ut.setup();
+
+    wr_0 = 0;
+    wdata_0 = 0;
+    waddr_0 = 0;
+
+    raddr_0 = 0;
+
+    wr_1 = 0;
+    wdata_1 = 0;
+    waddr_1 = 0;
+
+    raddr_1 = 0;
+
     /* Place Setup Code Here */
+    rst_n = 0;
+
+    step();
+
+    rst_n = 1;
+
+    step();
   endtask
 
 
@@ -70,7 +201,6 @@ module dpram_unit_test;
     svunit_ut.teardown();
     /* Place Teardown Code Here */
   endtask
-
 
   //===================================
   // All tests are defined between the
@@ -87,7 +217,13 @@ module dpram_unit_test;
   //===================================
   `SVUNIT_TESTS_BEGIN
 
+    `write_read_port_N_test(`PORT0)
+    `write_read_port_N_test(`PORT1)
 
+    `rdata_N_is_registered_test(`PORT0)
+    `no_write_N_in_reset(`PORT0)
+    `no_read_N_in_reset(`PORT0)
+    `write_read_port_N_full_range(`PORT0)
 
   `SVUNIT_TESTS_END
 
