3156056262c0aba8a1b7ba5d15b6b2956264e566
nosnhojn <nosnhojn@gmail.com>
2014-08-15 18:45:46 -0400

    dpram done checkpoint

--- hw/Vivado/hwIPBlock/hwIPBlock.srcs/sources_1/new/unitTest/dpram_unit_test.sv
+++ hw/Vivado/hwIPBlock/hwIPBlock.srcs/sources_1/new/unitTest/dpram_unit_test.sv
@@ -5,52 +5,52 @@
 
 `define write_read_port_N_test(PORT) \
 `SVTEST(write_read_port_``PORT) \
-  write_``PORT('hf, 'haabbccdd); \
+  write_port(PORT, 'hf, 'haabbccdd); \
   step(); \
-  read_``PORT('hf); \
+  read(PORT, 'hf); \
   step(); \
-  expect_rdata_``PORT('haabbccdd); \
+  expect_rdata(PORT, 'haabbccdd); \
 `SVTEST_END
 
 `define rdata_N_is_registered_test(PORT) \
 `SVTEST(rdata_``PORT``_is_registered) \
-  write_``PORT('hf, 'haabbccdd); \
+  write_port(PORT, 'hf, 'haabbccdd); \
   step(); \
-  read_``PORT('hf); \
-  expect_rdata_``PORT('hx); \
+  read(PORT, 'hf); \
+  expect_rdata(PORT, 'hx); \
 `SVTEST_END
 
 `define no_write_N_in_reset(PORT) \
 `SVTEST(no_write_``PORT``_in_reset) \
   rst_n = 0; \
-  write_``PORT('hf, 'haabbccdd); \
+  write_port(PORT, 'hf, 'haabbccdd); \
   step(); \
-  read_``PORT('hf); \
+  read(PORT, 'hf); \
   rst_n = 1; \
   step(); \
-  expect_rdata_``PORT('hx); \
+  expect_rdata(PORT, 'hx); \
 `SVTEST_END
 
 `define no_read_N_in_reset(PORT) \
 `SVTEST(no_read_``PORT``_in_reset) \
-  write_``PORT('hf, 'haabbccdd); \
+  write_port(PORT, 'hf, 'haabbccdd); \
   step(); \
-  read_``PORT('hf); \
+  read(PORT, 'hf); \
   rst_n = 0; \
   step(); \
-  expect_rdata_``PORT('hx); \
+  expect_rdata(PORT, 'hx); \
 `SVTEST_END
 
 `define write_read_port_N_full_range(PORT) \
 `SVTEST(write_read_port_``PORT``_full_range) \
   for (int i=0; i<`TEST_DPRAM_SIZE; i++) begin \
-    write_``PORT(i, i); \
+    write_port(PORT, i, i); \
     step(); \
   end \
   for (int i=0; i<`TEST_DPRAM_SIZE; i++) begin \
-    read_``PORT(i); \
+    read(PORT, i); \
     step(); \
-    expect_rdata_``PORT(i); \
+    expect_rdata(PORT, i); \
   end \
 `SVTEST_END
 
@@ -66,19 +66,12 @@ module dpram_unit_test;
   logic clk;
   logic rst_n;
 
-  logic [31:0] wdata_0;
-  logic [31:0] waddr_0;
-  logic wr_0;
+  logic [31:0] wdata [1:0];
+  logic [31:0] waddr [1:0];
+  logic        wr    [1:0];
 
-  logic [31:0] wdata_1;
-  logic [31:0] waddr_1;
-  logic wr_1;
-
-  wire  [31:0] rdata_0;
-  logic [31:0] raddr_0;
-
-  wire  [31:0] rdata_1;
-  logic [31:0] raddr_1;
+  wire  [31:0] rdata [1:0];
+  logic [31:0] raddr [1:0];
 
   //===================================
   // This is the UUT that we're 
@@ -93,19 +86,19 @@ module dpram_unit_test;
     .clk(clk),
     .rst_n(rst_n),
 
-    .wdata_0(wdata_0),
-    .waddr_0(waddr_0),
-    .wr_0(wr_0),
+    .wdata_0(wdata[0]),
+    .waddr_0(waddr[0]),
+    .wr_0(wr[0]),
 
-    .rdata_0(rdata_0),
-    .raddr_0(raddr_0),
+    .rdata_0(rdata[0]),
+    .raddr_0(raddr[0]),
 
-    .wdata_1(wdata_1),
-    .waddr_1(waddr_1),
-    .wr_1(wr_1),
+    .wdata_1(wdata[1]),
+    .waddr_1(waddr[1]),
+    .wr_1(wr[1]),
 
-    .rdata_1(rdata_1),
-    .raddr_1(raddr_1)
+    .rdata_1(rdata[1]),
+    .raddr_1(raddr[1])
   );
 
   initial begin
@@ -121,38 +114,21 @@ module dpram_unit_test;
     end
   endtask
 
-  task expect_rdata_0(logic [31:0] exp);
+  task expect_rdata(int port, logic [31:0] exp);
     if ($time%5 == 0) #1;
-    `FAIL_UNLESS(rdata_0 === exp);
+    `FAIL_UNLESS(rdata[port] === exp);
   endtask
 
-  task write_0(bit [31:0] addr, bit [31:0] data);
+  task write_port(int port, bit [31:0] addr, bit [31:0] data);
     if ($time%5 == 0) #1;
-    wr_0 = 1;
-    waddr_0 = addr;
-    wdata_0 = data;
+    wr[port] = 1;
+    waddr[port] = addr;
+    wdata[port] = data;
   endtask
 
-  task read_0(bit [31:0] addr);
+  task read(int port, bit [31:0] addr);
     if ($time%5 == 0) #1;
-    raddr_0 = addr;
-  endtask
-
-  task expect_rdata_1(logic [31:0] exp);
-    if ($time%5 == 0) #1;
-    `FAIL_UNLESS(rdata_1 === exp);
-  endtask
-
-  task write_1(bit [31:0] addr, bit [31:0] data);
-    if ($time%5 == 0) #1;
-    wr_1 = 1;
-    waddr_1 = addr;
-    wdata_1 = data;
-  endtask
-
-  task read_1(bit [31:0] addr);
-    if ($time%5 == 0) #1;
-    raddr_1 = addr;
+    raddr[port] = addr;
   endtask
 
 
@@ -170,17 +146,13 @@ module dpram_unit_test;
   task setup();
     svunit_ut.setup();
 
-    wr_0 = 0;
-    wdata_0 = 0;
-    waddr_0 = 0;
+    for (int port=0; port<2; port+=1) begin
+      wr[port] = 0;
+      wdata[port] = 0;
+      waddr[port] = 0;
 
-    raddr_0 = 0;
-
-    wr_1 = 0;
-    wdata_1 = 0;
-    waddr_1 = 0;
-
-    raddr_1 = 0;
+      raddr[port] = 0;
+    end
 
     /* Place Setup Code Here */
     rst_n = 0;
@@ -221,9 +193,16 @@ module dpram_unit_test;
     `write_read_port_N_test(`PORT1)
 
     `rdata_N_is_registered_test(`PORT0)
+    `rdata_N_is_registered_test(`PORT1)
+
     `no_write_N_in_reset(`PORT0)
+    `no_write_N_in_reset(`PORT1)
+
     `no_read_N_in_reset(`PORT0)
+    `no_read_N_in_reset(`PORT1)
+
     `write_read_port_N_full_range(`PORT0)
+    `write_read_port_N_full_range(`PORT1)
 
   `SVUNIT_TESTS_END
 
